0651898324e57ef17aaa0c08736410de6ea60674
AntonioFBove <antoniofbove@gmail.com>
2022-01-12 18:06:02 +0100

    milestone 3

--- js/script.js
+++ js/script.js
@@ -122,13 +122,64 @@ function getIconHtmlTemplate(iconInfo) {
   `;
 }
 
-const container = document.getElementById('container');
+
+
+const container = document.getElementById('icons-container');
 
 iconsList.forEach(element => {
   const newBox = document.createElement('div');
 
-  newBox.className = 'box';
+  newBox.className = 'box show';
   newBox.innerHTML += getIconHtmlTemplate(element);
 
   container.append(newBox);
 });
+
+
+const selectHtml = document.getElementById('type-select');
+
+selectHtml.addEventListener('change', function(event) {
+  let input = event.target.value;
+  let filteredList = [];
+  
+  if (input === 'all') {
+    filteredList = iconsList;
+  } else {
+    filteredList = iconsList.filter((icon) => {
+      return icon.type === input
+    })
+  }
+
+  /*
+  qui sarebbe stato più facile aggiungere il tipo di icona
+  alle classi dell'<i>, e paragonare direttamente
+
+  invece ho perso un sacco di tempo per provare a fare tutto
+  dentro js
+   */
+
+  const showNames = filteredList.map((icon) => {
+    return icon.name
+  })
+  const boxesHtml = container.querySelectorAll('.box');
+  
+  boxesHtml.forEach((box) => {
+    // aggiungo la classe show nel caso fosse stata rimossa in precedenza
+    box.classList.add('show')
+
+    let show = false;
+    
+    const iconClasses = box.querySelector('i').classList;
+
+    // verifico se le classi di <i> contengono il nome dell'icona che rimane dopo il filtro
+    showNames.forEach(name => {
+      if (iconClasses.contains(`fa-${name}`)) {
+        show = true;
+      }
+    });
+
+    if (!show) {
+      box.classList.remove('show')
+    }
+  });
+})
